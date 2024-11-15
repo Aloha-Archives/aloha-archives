e8e8c52f905400765f42678a8e4a25e0bd45b908
jswlee <jswleetaiwan@gmail.com>
2024-11-15 07:13:08 -1000

    Update AboutUsText.tsx

--- src/components/text/AboutUsText.tsx
+++ src/components/text/AboutUsText.tsx
@@ -8,7 +8,7 @@ const AboutUsText = () => (
       Organized and captained by JR Lee, the team members include Jared Seto, Shaelyn Loo, and
       Kevin Clarkin. Sharing a class called ICS 314, we named ourselves ICS 3+1=4, since there
       were 3 of us, we added 1 more, and we are Section 4. Only after countless hours (and some
-      head banging one the table) did we finally arrive at (the much catchier) Aloha Archives,
+      head banging on the table) did we finally arrive at (the much catchier) Aloha Archives,
       thanks to a moment of inspiration by Shaelyn. We have carried the name with us ever since.
     </p>
   </Container>
