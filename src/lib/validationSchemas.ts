bd865bd2d40efd237d22e821c658ae4ee5a7d7e1
Jared Seto <jseto2018@gmail.com>
2024-11-18 11:34:27 -1000

    Update Datasets to include date

--- src/lib/validationSchemas.ts
+++ src/lib/validationSchemas.ts
@@ -25,4 +25,5 @@ export const EditDatasetSchema = Yup.object().shape({
   topic: Yup.string().required('Topic is required'),
   description: Yup.string().required('Description is required'),
   org: Yup.string().required('Organization is required'),
+  date: Yup.date().required('Date is required'),
 });
